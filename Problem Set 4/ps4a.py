2719830fefc0a3cc1191cd9818d67ee49fc1aef9
D <laxos90@gmail.com>
2015-11-22 19:14:15 -0500

    Start pset 4.

--- Problem Set 4/ps4a.py
+++ Problem Set 4/ps4a.py
@@ -75,12 +75,7 @@ def get_word_score(word, n):
     :param n: integer (HAND_SIZE; i.e., hand size required for additional points)
     :returns: the score for a word. Assumes the word is a valid word. (int >= 0)
     """
-    score = 0
-
-    for letter in word:
-        score += SCRABBLE_LETTER_VALUES[letter]
-
-    return score * len(word) + 50 * (len(word) == n)
+    pass
 
 
 #
@@ -98,10 +93,7 @@ def display_hand(hand):
 
     :param hand: dictionary (string -> int)
     """
-    for letter in hand.keys():
-        for j in range(hand[letter]):
-            print(letter, end=" ")  # print all on the same line
-    print()  # print an empty line
+    pass
 
 
 #
@@ -117,19 +109,7 @@ def deal_hand(n):
     :returns: dictionary (string -> int). Returns a random hand containing
     n lowercase letters. At least n/3 the letters in the hand should be VOWELS.
     """
-    hand = {}
-    num_vowels = n // 3
-
-    for i in range(num_vowels):
-        x = VOWELS[random.randrange(len(VOWELS))]
-        hand[x] = hand.get(x, 0) + 1
-
-    for i in range(num_vowels, n):
-        x = CONSONANTS[random.randrange(len(CONSONANTS))]
-        hand[x] = hand.get(x, 0) + 1
-
-    return hand
-
+    pass
 
 #
 # Problem #2: Update a hand by removing letters
@@ -150,13 +130,7 @@ def update_hand(hand, word):
     :param hand: dictionary (string -> int)
     :returns: dictionary (string -> int).
     """
-    updated_hand = hand.copy()
-    letter_frequency = get_frequency_dict(word)
-
-    for letter in letter_frequency:
-        updated_hand[letter] -= letter_frequency[letter]
-
-    return updated_hand
+    pass
 
 
 #
@@ -172,13 +146,7 @@ def is_valid_word(word, hand, word_list):
     :returns: True if word is in the word_list and is entirely
     composed of letters in the hand. Otherwise, returns False.
     """
-    letter_frequency = get_frequency_dict(word)
-
-    for letter in letter_frequency:
-        if hand.get(letter, 0) < letter_frequency[letter]:
-            return False
-
-    return word in word_list
+    pass
 
 
 #
@@ -190,30 +158,7 @@ def calculate_hand_len(hand):
     :param hand: dictionary (string-> int)
     :returns: the length (number of letters) in the current hand.
     """
-    number_of_letters = 0
-
-    for letter in hand:
-        number_of_letters += hand[letter]
-
-    return number_of_letters
-
-
-def print_current_hand(hand):
-    print("Current Hand:", end=" ")
-    display_hand(hand)
-
-
-def print_total_score(score):
-    print("Total score: " + str(score) + " points.")
-
-
-def update_and_print_score(score, word, n):
-    current_score = get_word_score(word, n)
-    score += current_score
-    print("\"" + word + "\" earned " + str(current_score) + " points.", end=" ")
-    print_total_score(score)
-
-    return score
+    pass
 
 
 def play_hand(hand, word_list, n):
@@ -237,44 +182,12 @@ def play_hand(hand, word_list, n):
     :param word_list: list of lowercase strings
     :param n: integer (HAND_SIZE; i.e., hand size required for additional points)
     """
-    score = 0
-    while calculate_hand_len(hand):
-        print_current_hand(hand)
-        word = input(ENTER_WORD_PROMPT)
-
-        if word == ".":
-            print(BYE, end=" ")
-            break
-
-        if is_valid_word(word, hand, word_list):
-            score += update_and_print_score(score, word, n)
-            hand = update_hand(hand, word)
-        else:
-            print(INVALID_WORD_MESSAGE)
-        print()
-
-    if calculate_hand_len(hand) == 0:
-        print(OUT_OF_LETTERS, end=" ")
-
-    print_total_score(score)
-
-
-def get_initial_valid_input():
-    action = input(PLAY_GAME_PROMPT)
-    # avoids user entering r when he hasn't played before
-    while action != "n" and action != "e":
-        if action == "r":
-            print(NO_HAND_ENTERED_YET)
-        else:
-            print(INVALID_COMMAND_MESSAGE)
-        action = input(PLAY_GAME_PROMPT)
-
-    return action
+    pass
 
 
 #
 # Problem #5: Playing a game
-# 
+#
 
 def play_game(word_list):
     """
@@ -289,18 +202,7 @@ def play_game(word_list):
     2) When done playing the hand, repeat from step 1    
     :param word_list: list of lowercase strings
     """
-    action = get_initial_valid_input()
-
-    while action != "e":
-        if action != "n" and action != "r":
-            print(INVALID_COMMAND_MESSAGE)
-        else:
-            if action == "n":
-                hand = deal_hand(HAND_SIZE)
-            play_hand(hand, word_list, HAND_SIZE)
-            print()
-
-        action = input(PLAY_GAME_PROMPT)
+    pass
 
 
 def main():
