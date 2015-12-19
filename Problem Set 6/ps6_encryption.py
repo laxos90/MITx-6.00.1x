8debd339b83cfdb4663a23f8d82547b76ac9a74d
D <laxos90@gmail.com>
2015-12-19 17:48:13 -0500

    Finish pset 6

--- Problem Set 6/ps6_encryption.py
+++ Problem Set 6/ps6_encryption.py
@@ -114,11 +114,13 @@ def build_coder(shift):
 def add_shifted_lowercase_letters(dictionary, shift):
     lowercase_letters = string.ascii_lowercase
     for letter in lowercase_letters:
-        dictionary[letter] = letter
+        dictionary[letter] = lowercase_letters[(lowercase_letters.index(letter) + shift) % len(lowercase_letters)]
 
 
 def add_shifted_uppercase_letters(dictionary, shift):
-    pass
+    uppercase_letters = string.ascii_uppercase
+    for letter in uppercase_letters:
+        dictionary[letter] = uppercase_letters[(uppercase_letters.index(letter) + shift) % len(uppercase_letters)]
 
 
 def apply_coder(text, coder):
@@ -130,6 +132,14 @@ def apply_coder(text, coder):
     :returns: text after mapping coder chars to original text
     """
 
+    cypher_text = ""
+    for char in text:
+        if char in string.ascii_letters:
+            cypher_text += coder[char]
+        else:
+            cypher_text += char
+
+    return cypher_text
 
 
 def apply_shift(text, shift):
@@ -144,11 +154,7 @@ def apply_shift(text, shift):
     :returns: text after being shifted by specified amount.
     """
 
-
-
-def get_number_of_valid_words(words, word_list):
-    number_of_valid_words = 0
-
+    return apply_coder(text, build_coder(shift))
 
 
 #
@@ -163,6 +169,27 @@ def find_best_shift(word_list, text):
     :returns: 0 <= int < 26
     """
 
+    valid_key = 0
+    number_of_valid_words = 0
+
+    for key in range(26):
+        current_number_of_valid_words = get_number_of_valid_words(word_list, apply_shift(text, key))
+        if current_number_of_valid_words > number_of_valid_words:
+            number_of_valid_words = current_number_of_valid_words
+            valid_key = key
+
+    return valid_key
+
+
+def get_number_of_valid_words(word_list, decyphered_text):
+    number_of_valid_words = 0
+    words = decyphered_text.split()
+
+    for word in words:
+        if is_word(word_list, word):
+            number_of_valid_words += 1
+
+    return number_of_valid_words
 
 
 def decrypt_story():
@@ -174,10 +201,16 @@ def decrypt_story():
 
     :returns: string - story in plain text
     """
+    story = get_story_string()
+    word_list = load_words()
+    best_shift = find_best_shift(word_list, get_story_string())
+
+    return apply_shift(story, best_shift)
 
 #
 # Build data structures used for entire session and run encryption
 #
+
 
 def main():
     print(decrypt_story())
